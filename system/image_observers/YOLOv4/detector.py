fix mqtt unsubscribe bug; change image timestamps to float in sec units; fix yolo observer bugs

system/image_observers/YOLOv4/detector.py
@@ -110,13 +110,13 @@ class YOLOv4Detector:
         darknet.free_detections(dets, num)
 
         if res.shape[0] == 0:
-            return []
-        
+            return None
+
         if self.return_neareast_detection:
             if self.prev_bbox is None:
                 self.prev_bbox = res[np.argmax(res[:, 4])]
             else:
                 self.prev_bbox = bbox.nearest_bbox(res, bbox.xyxy_to_centroid(self.prev_bbox))
-            return self.prev_bbox.tolist()
+            return self.prev_bbox
         else:
-            return res.tolist()
+            return res

system/video_stream.py
@@ -28,7 +28,7 @@ class ImageSource(mp.Process):
             self.buf_shape
         )
 
-        self.timestamp = mp.Value("L")
+        self.timestamp = mp.Value("d")
         self.end_event = mp.Event()  # do we really need two events? v--
         self.stop_event = mp.Event()
 
@@ -84,6 +84,7 @@ class ImageSource(mp.Process):
             while True:
                 try:
                     img, timestamp = self.acquire_image()
+
                 except AcquireException as e:
                     self.log.error(e)
                     break
@@ -98,6 +99,7 @@ class ImageSource(mp.Process):
 
                 with self.buf.get_lock():
                     self.timestamp.value = timestamp
+
                     self.buf_np = np.frombuffer(
                         self.buf.get_obj(), dtype="uint8"
                     ).reshape(self.buf_shape)