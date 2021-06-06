a1caa6bb25b2e296eaee368c51b11bf38fcd3424
eisental <eisental@gmail.com>
2021-06-06 20:04:32 +0300

    monitor.py: docs

--- system/monitor.py
+++ system/monitor.py
@@ -1,19 +1,23 @@
+"""
+Monitor hardware controller
+Author: Or Pardilov, 2021
+
+Provides convenience functions for communicating with the monitor program over MQTT
+"""
 import mqtt
-import schedule
 
 
 def change_color(color="random"):
+    #if a color is not given, let the application choose a random color
     mqtt.client.publish(topic="monitor/color", payload=color)
 
 
-def show_image(path=""):
-    if path:
-        mqtt.client.publish(topic="monitor/image", payload=path)
+def show_image(path):
+    mqtt.client.publish(topic="monitor/image", payload=str(path))
 
 
-def play_video(path=""):
-    if path:
-        mqtt.client.publish(topic="monitor/play_video", payload=path)
+def play_video(path):
+    mqtt.client.publish(topic="monitor/play_video", payload=str(path))
 
 
 def stop_video():
