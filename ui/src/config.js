97e234d084f09eb158e29b5c5f778b15cacab39e
eisental <eisental@gmail.com>
2021-06-09 22:12:50 +0300

    support for both tunneling and direct access

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,2 +1,2 @@
-export const api_url = "http://localhost:5000"
-export const socketio_url = "localhost:5000"
+export const api_url = `http://${window.location.hostname}:5000`;
+export const socketio_url = `${window.location.hostname}:5000`;
