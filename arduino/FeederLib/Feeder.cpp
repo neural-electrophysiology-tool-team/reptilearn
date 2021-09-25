c69187bd751cc0a5b54164242e1e32dc013973e6
eisental <eisental@gmail.com>
2021-09-25 19:14:42 +0300

    feeder code encapsulated in an arduino lib

---
+++ arduino/FeederLib/Feeder.cpp
@@ -0,0 +1,62 @@
+#include "Arduino.h"
+#include "Feeder.h"
+
+Feeder::Feeder(int motor1, int motor2, int motor3, int motor4) :
+  stepper(AccelStepper(MOTOR_INTERFACE_TYPE, motor1, motor3, motor2, motor4)),
+  state(full_backward)
+{
+}
+
+void Feeder::init() {
+  state = full_backward;
+  stepper.setMaxSpeed(1000);
+  stepper.setCurrentPosition(0);
+  stepper.setSpeed(-STEPPER_SPEED);
+}
+
+void Feeder::loop() {
+  if (state == forward) {
+    if (stepper.currentPosition() != 5096) {
+      stepper.runSpeed();
+    }
+    else {
+      state = short_backward;
+      stepper.setSpeed(-STEPPER_SPEED);
+    }
+  }
+  else if (state == short_backward) {
+    if (stepper.currentPosition() != 3000) {
+      stepper.runSpeed();
+    }
+    else {
+      state = prepare;
+      stepper.setCurrentPosition(0);
+      stepper.setSpeed(STEPPER_SPEED);
+    }
+  }
+  else if (state == prepare) {
+    if (stepper.currentPosition() != 4000) {
+      stepper.runSpeed();
+    }
+    else {
+      state = standby;
+    }
+  }
+  else if (state == full_backward) {
+    if (stepper.currentPosition() != -4096) {
+      stepper.runSpeed();
+    }
+    else {
+      state = prepare;
+      stepper.setCurrentPosition(0);
+      stepper.setSpeed(STEPPER_SPEED);      
+    } 
+  }
+}
+
+void Feeder::feed() {
+  if (state == standby) {
+    state = forward;     
+    stepper.setSpeed(STEPPER_SPEED);
+  }
+}
