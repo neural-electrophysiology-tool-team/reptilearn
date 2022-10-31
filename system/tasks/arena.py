28ac5f8b234009a653751d78eff8e393b51d5b13
NitzanAlbeck <nitzan.albeck@gmail.com>
2022-10-31 15:18:44 +0200

    adding IR lights tasks

--- system/tasks/arena.py
+++ system/tasks/arena.py
@@ -6,12 +6,22 @@ log = get_main_logger()
 
 def lights_on():
     log.info("Turning lights on")
-    arena.run_command("set", "AC Line 2", [1])
+    arena.run_command("set", "Day lights", [1])
 
 
 def lights_off():
     log.info("Turning lights off")
-    arena.run_command("set", "AC Line 2", [0])
+    arena.run_command("set", "Day lights", [0])
+
+
+def IRlights_on(log):
+    log.info("Turning IR lights on")
+    arena.run_command("set", "Night lights", [1])
+
+
+def IRlights_off(log):
+    log.info("Turning IR lights off")
+    arena.run_command("set", "Night lights", [0])
 
 
 def heat_on():
